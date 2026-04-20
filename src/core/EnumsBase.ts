90f56101607fbc4872cf450a5278504460ff565b
Anil <d.anilchandravarma@gmail.com>
2026-04-20 12:21:53 +0530

    inital data

---
+++ src/core/EnumsBase.ts
@@ -0,0 +1,48 @@
+export enum PAGING {
+  Yes = 'Yes',
+  No = 'No',
+}
+
+export enum LoadParents {
+  Yes = 'Yes',
+  No = 'No',
+  Custom = 'Custom',
+}
+
+export enum LoadChild {
+  No = 'No',
+  Yes = 'Yes',
+  Count = 'Count',
+  Custom = 'Custom',
+  Direct = 'Direct',
+}
+
+export enum LoadChildCount {
+  No = 'No',
+  Yes = 'Yes',
+  Custom = 'Custom',
+}
+
+export enum OrderBy {
+  asc = 'asc',
+  desc = 'desc',
+}
+
+export enum AdminRole {
+  MasterAdmin = 'MasterAdmin',
+  Admin = 'Admin',
+}
+
+export enum LoginFrom {
+  Web = 'Web',
+  Android = 'Android',
+  IPhone = 'IPhone',
+  AndroidPWA = 'AndroidPWA',
+  iOSPWA = 'iOSPWA',
+}
+
+export enum Status {
+  Active = 'Active',
+  Inactive = 'Inactive',
+}
+
